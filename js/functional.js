124a1e23d9a25e170012389dcf4e915d1c075d81
rahathossain3 <rahathossain210@gmail.com>
2022-02-22 15:15:07 +0600

    querySelectorAll

--- js/functional.js
+++ js/functional.js
@@ -1,38 +1,61 @@
-function getInputValue(fieldID) {
-    const inputField = document.getElementById(fieldID);
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId);
     const valueInText = inputField.value;
     const value = parseFloat(valueInText);
     inputField.value = '';
     return value;
 }
 
-function updateTotal(fieldID, amount) {
-
-    const totalTag = document.getElementById(fieldID);
-    const previousTotalInText = totalTag.innerText;
-    const previousTotal = parseFloat(previousTotalInText);
-    const newTotal = previousTotal + amount;
-    totalTag.innerText = newTotal;
-    return newTotal;
-
+function getInnerTextValue(fieldId) {
+    const fieldTag = document.getElementById(fieldId);
+    const fieldValueInText = fieldTag.innerText;
+    const value = parseFloat(fieldValueInText);
+    return value;
 }
-function updateBalance() {
-    const balanceTag = document.getElementById('balance-total');
-    const balanceInText = balanceTag.innerText;
-    const previousBalance = parseFloat(balanceInText);
+
+function updateTotal(fieldId, amount) {
+    // const totalTag = document.getElementById(fieldId);
+    // const previousTotalInText = totalTag.innerText;
+    // const previousTotal = parseFloat(previousTotalInText);
+    const previousTotal = getInnerTextValue(fieldId);
+    const newTotal = previousTotal + amount;
+    // totalTag.innerText = newTotal;
+    document.getElementById(fieldId).innerText = newTotal;
+    return newTotal;
+}
+
+function updateBalance(amount, isAdding) {
+    // const balanceTag = document.getElementById('balance-total');
+    // const balanceInText = balanceTag.innerText;
+    // const previousBalance = parseFloat(balanceInText);
+    const previousBalance = getInnerTextValue('balance-total');
+    let newBalance;
+    if (isAdding == true) {
+        newBalance = previousBalance + amount;
+    }
+    else {
+        newBalance = previousBalance - amount;
+    }
+    // balanceTag.innerText = newBalance;
+    document.getElementById('balance-total').innerText = newBalance;
 }
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
-    updateTotal('deposit-total', amount)
-
+    if (amount > 0) {
+        updateTotal('deposit-total', amount);
+        updateBalance(amount, true);
+    }
     // console.log(amount);
 })
 
 // handel withdraw 
 document.getElementById('withdraw-button').addEventListener('click', function () {
     const amount = getInputValue('withdraw-input');
-    updateTotal('withdraw-total', amount);
-
+    const balance = getInnerTextValue('balance-total')
+    if (amount > 0 && amount <= balance) {
+        updateTotal('withdraw-total', amount);
+        updateBalance(amount, false)
+    }
 
 })
